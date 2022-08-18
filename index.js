60fcaae5ffbd4fc49b853d17428677e85fa7783f
Valentine Kalchevski <valentin.kalchevsky@yellow.systems>
2022-08-18 11:23:45 +0300

    single commit

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function add (a=3,b=3) {
     console.log(a + b)
     console.log('reature One')
 }
+
+
+console.log('first commit');
+console.log('first commit');
+console.log('first commit');
+console.log('first commit');
+console.log('first commit');
+console.log('first commit');
+
